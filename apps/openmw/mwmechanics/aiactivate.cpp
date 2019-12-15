uramer/openmw

commit 48aba76ce904738d428e79f1ee24ce170f2a8309

Implement vanilla-style AiActivate (bug #4456)

apps/openmw/mwmechanics/aiactivate.cpp
@@ -9,6 +9,7 @@
 
 #include "creaturestats.hpp"
 #include "movement.hpp"
+#include "steering.hpp"
 
 namespace MWMechanics
 {
@@ -33,16 +34,18 @@ namespace MWMechanics
         if (target == MWWorld::Ptr() || !target.getRefData().getCount() || !target.getRefData().isEnabled())
             return true;
 
-        //Set the target destination for the actor
-        const osg::Vec3f dest = target.getRefData().getPosition().asVec3();
+        // Turn to target and move to it directly, without pathfinding.
+        const osg::Vec3f targetDir = target.getRefData().getPosition().asVec3() - actor.getRefData().getPosition().asVec3();
 
-        if (pathTo(actor, dest, duration, MWBase::Environment::get().getWorld()->getMaxActivationDistance())) //Stop when you get in activation range
+        zTurn(actor, std::atan2(targetDir.x(), targetDir.y()), 0.f);
+        actor.getClass().getMovementSettings(actor).mPosition[1] = 1;
+        actor.getClass().getMovementSettings(actor).mPosition[0] = 0;
+
+        if (MWBase::Environment::get().getWorld()->getMaxActivationDistance() >= targetDir.length())
         {
-            // activate when reached
+            // Note: we intentionally do not cancel package after activation here for backward compatibility with original engine.
             MWBase::Environment::get().getWorld()->activate(target, actor);
-            return true;
         }
-
         return false;
     }
 

commit 9763f6f9c55329874878339637935a679c905655

[Client] Fix infinite activation loops caused by 48aba76ce904738d428e79f1ee24ce170f2a8309

apps/openmw/mwmechanics/aiactivate.cpp
@@ -77,19 +77,6 @@ namespace MWMechanics
 
         if (MWBase::Environment::get().getWorld()->getMaxActivationDistance() >= targetDir.length())
         {
-            // Note: we intentionally do not cancel package after activation here for backward compatibility with original engine.
-
-            /*
-                Start of tes3mp change (major)
-
-                Disable unilateral activation on this client and expect the server's reply to our
-                packet to do it instead
-            */
-            //MWBase::Environment::get().getWorld()->activate(target, actor);
-            /*
-                End of tes3mp change (major)
-            */
-
             /*
                 Start of tes3mp addition
 
@@ -102,6 +89,21 @@ namespace MWMechanics
             objectList->sendObjectActivate();
             /*
                 End of tes3mp addition
+            */
+
+            /*
+                Start of tes3mp change (major)
+
+                Disable unilateral activation on this client and expect the server's reply to our
+                packet to do it instead
+
+                Cancel the package to avoid an infinite activation loop, deviating from the behavior
+                established in OpenMW in commit 48aba76ce904738d428e79f1ee24ce170f2a8309
+            */
+            //MWBase::Environment::get().getWorld()->activate(target, actor);
+            return true;
+            /*
+                End of tes3mp change (major)
             */
         }
         return false;