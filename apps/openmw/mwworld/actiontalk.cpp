2249450b0efd523f09182087ef296bda581bfc20
David Cernat <davidcernat@gmail.com>
2019-12-15 11:50:15 +0200

    [Client] Fix inability for actors to talk to players caused by 4118b20608b630b8d166d060a34c1234b80e378d

--- apps/openmw/mwworld/actiontalk.cpp
+++ apps/openmw/mwworld/actiontalk.cpp
@@ -11,7 +11,15 @@ namespace MWWorld
 
     void ActionTalk::executeImp (const Ptr& actor)
     {
-        if (actor == MWMechanics::getPlayer())
-            MWBase::Environment::get().getWindowManager()->pushGuiMode(MWGui::GM_Dialogue, getTarget());
+        /*
+            Start of tes3mp change (major)
+
+            We need to be able to make actors start conversations with players, so reverse the check added
+            by 4118b20608b630b8d166d060a34c1234b80e378d here
+        */
+        MWBase::Environment::get().getWindowManager()->pushGuiMode(MWGui::GM_Dialogue, getTarget());
+        /*
+            End of tes3mp change (major)    
+        */
     }
 }

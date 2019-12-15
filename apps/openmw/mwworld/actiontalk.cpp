uramer/openmw

commit 4118b20608b630b8d166d060a34c1234b80e378d

Allow ActionOpen and ActionTalk only for player (bug #5210

apps/openmw/mwworld/actiontalk.cpp
@@ -3,12 +3,15 @@
 #include "../mwbase/environment.hpp"
 #include "../mwbase/windowmanager.hpp"
 
+#include "../mwmechanics/actorutil.hpp"
+
 namespace MWWorld
 {
     ActionTalk::ActionTalk (const Ptr& actor) : Action (false, actor) {}
 
     void ActionTalk::executeImp (const Ptr& actor)
     {
-        MWBase::Environment::get().getWindowManager()->pushGuiMode(MWGui::GM_Dialogue, getTarget());
+        if (actor == MWMechanics::getPlayer())
+            MWBase::Environment::get().getWindowManager()->pushGuiMode(MWGui::GM_Dialogue, getTarget());
     }
 }

apps/openmw/mwmechanics/aipursue.cpp
@@ -3,6 +3,7 @@
 #include <components/esm/aisequence.hpp>
 
 #include "../mwbase/environment.hpp"
+#include "../mwbase/windowmanager.hpp"
 #include "../mwbase/world.hpp"
 
 #include "../mwworld/class.hpp"
@@ -58,7 +59,7 @@ bool AiPursue::execute (const MWWorld::Ptr& actor, CharacterController& characte
     if (pathTo(actor, dest, duration, pathTolerance) &&
         std::abs(dest.z() - actorPos.z()) < pathTolerance) // check the true distance in case the target is far away in Z-direction
     {
-        target.getClass().activate(target,actor).get()->execute(actor); //Arrest player when reached
+        MWBase::Environment::get().getWindowManager()->pushGuiMode(MWGui::GM_Dialogue, actor); //Arrest player when reached
         return true;
     }
 

apps/openmw/mwworld/actionopen.cpp
@@ -18,6 +18,9 @@ namespace MWWorld
         if (!MWBase::Environment::get().getWindowManager()->isAllowed(MWGui::GW_Inventory))
             return;
 
+        if (actor != MWMechanics::getPlayer())
+            return;
+
         if (!MWBase::Environment::get().getMechanicsManager()->onOpen(getTarget()))
             return;
 

commit 2249450b0efd523f09182087ef296bda581bfc20

[Client] Fix inability for actors to talk to players caused by 4118b20608b630b8d166d060a34c1234b80e378d

apps/openmw/mwworld/actiontalk.cpp
@@ -11,7 +11,15 @@ namespace MWWorld
 
     void ActionTalk::executeImp (const Ptr& actor)
     {
-        if (actor == MWMechanics::getPlayer())
-            MWBase::Environment::get().getWindowManager()->pushGuiMode(MWGui::GM_Dialogue, getTarget());
+        /*
+            Start of tes3mp change (major)
+
+            We need to be able to make actors start conversations with players, so reverse the check added
+            by 4118b20608b630b8d166d060a34c1234b80e378d here
+        */
+        MWBase::Environment::get().getWindowManager()->pushGuiMode(MWGui::GM_Dialogue, getTarget());
+        /*
+            End of tes3mp change (major)    
+        */
     }
 }